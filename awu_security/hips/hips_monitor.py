5b10e109f7b52ec6d5ee866bceb93ef269210cf9
abdullah-alzghoul <abdullahzghoul777@gmail.com>
2026-07-14 14:35:55 +0300

    update

--- awu_security/hips/hips_monitor.py
+++ awu_security/hips/hips_monitor.py
@@ -281,6 +281,10 @@ if __name__ == "__main__":
         while True:
             time.sleep(2)
     except KeyboardInterrupt:
+        print("\n[+] Shutting down, please wait...")
         observer.stop()
-        print("\n[+] HIPS stopped.")
-    observer.join()
+    try:
+        observer.join()
+    except KeyboardInterrupt:
+        pass
+    print("[+] HIPS stopped.")
